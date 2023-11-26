5e4ba11f87740bc92fea0b0245748752afa757fe
PUPOXWS <pupoxws@gmail.com>
2023-11-25 22:16:02 -0500

    Clase 25 Noviembre

---
+++ CLASE-01/index.js
@@ -0,0 +1,52 @@
+// let year = 2023;
+// const month = 11;
+// let day= 23;
+// let name = "Pupox";
+// let age = 25;
+// let employee = false;
+
+// day = day+1;
+
+// console.log(year);
+// console.log(month);
+// console.log("Año: " + year + " Mes " + month + " Dia: " + day);
+
+// document.getElementById("parrafo1").innerHTML = "Bienvenido " + name;
+// document.getElementById("parrafo2").innerHTML = "Edad "+ age;
+// document.getElementById("parrafo3").innerHTML = "Empleado " + employee;
+
+let price = 500;
+// price -= 50;
+// price *= 3;
+price /= 3;
+console.log(price);
+
+//Jerarquia de numeros
+let operation = (1 + 2) * (3 + 4);
+console.log(operation);
+
+// const ciudad = prompt("Donde vives?");
+
+let username;
+
+document.getElementById("button").onclick = function () {
+    username = document.getElementById("mitexto").value;
+    console.log(username);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
